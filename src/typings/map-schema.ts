afd741da90383461b5770d55a2a41c8aef319d35
DidaS <didasoficial@gmail.com>
2023-05-06 17:48:55 +0100

    Fixed broken types
    Im starting to wonder what im doing with my life

--- src/typings/map-schema.ts
+++ src/typings/map-schema.ts
@@ -2,9 +2,27 @@ import type { ReferenceArray } from "../utils";
 import type { FieldMap } from "./field-map";
 import type { ParseSchema } from "./parse-schema";
 
-export type MapSchema<T extends ParseSchema<any>, AF extends boolean = false> = MapSchemaData<T["data"]> & MapSchemaReferences<T["references"], AF>;
+export type MapSchema<T extends ParseSchema<any>, AF extends boolean = false, CAS extends boolean = false> = _MapSchema<T, AF, CAS>;
 
-type MapSchemaData<T extends ParseSchema<any>["data"]> = {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+type _MapSchema<
+    T extends ParseSchema<any>,
+    AF extends boolean = false,
+    CAS extends boolean = false
+> = MapSchemaData<T["data"], CAS> & (CAS extends true ? Partial<MapSchemaReferences<T["references"], AF, CAS>> : MapSchemaReferences<T["references"], AF, CAS>);
+
+type MapSchemaData<T extends ParseSchema<any>["data"], CAS extends boolean = false, SD = _MapSchemaData<T>> = (Pick<SD, ({
+    [K in keyof T]: T[K]["required"] extends true
+    ? K
+    : T[K]["default"] extends {}
+    ? CAS extends true
+    ? never
+    : K
+    : never
+}[keyof T]) & keyof SD> & Partial<SD>);
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+type _MapSchemaData<T extends ParseSchema<any>["data"]> = {
     [K in keyof T]: T[K] extends { properties: any }
     ? T[K]["properties"] extends ParseSchema<any> ? MapSchema<T[K]["properties"]> : unknown
     : T[K] extends { elements: any }
@@ -12,8 +30,10 @@ type MapSchemaData<T extends ParseSchema<any>["data"]> = {
     : FieldMap[T[K]["type"]]
 };
 
-type MapSchemaReferences<T extends ParseSchema<any>["references"], AF extends boolean> = {
-    [K in keyof T]: AF extends true
+type MapSchemaReferences<T extends ParseSchema<any>["references"], AF extends boolean = false, CAS extends boolean = false> = {
+    [K in keyof T]: CAS extends true
+    ? Array<string>
+    : AF extends true
     ? Array<MapSchema<T[K]["schema"]>>
     : ReferenceArray
 };
