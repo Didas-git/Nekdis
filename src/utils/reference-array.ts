87e829340cf9d7b0d8258455af9e43e2bb19d9a6
DidaS <didasoficial@gmail.com>
2023-05-07 20:15:35 +0100

    Improved reference helper

--- src/utils/reference-array.ts
+++ src/utils/reference-array.ts
@@ -5,8 +5,10 @@ import type { ParseSchema } from "../typings";
 export class ReferenceArray extends Array<string> {
     public reference(...recordOrDoc: Array<string | Document<ParseSchema<any>>>): this {
         for (let i = 0, len = recordOrDoc.length; i < len; i++) {
-            const temp = recordOrDoc[i];
-            this.push(temp instanceof Document ? temp.$record_id : temp);
+            const tempVal = recordOrDoc[i];
+            const tempId = tempVal instanceof Document ? tempVal.$record_id : tempVal;
+            if (this.includes(tempId)) continue;
+            this.push(tempId);
         }
         return this;
     }
