c8b85513a7ac1958aca95d187394311cb1be59c6
DidaS <didasoficial@gmail.com>
2023-08-31 01:10:17 +0100

    Fixed unknown index being added to document types

--- src/typings/return-doc.ts
+++ src/typings/return-doc.ts
@@ -7,4 +7,4 @@ export type Document = JSONDocument | HASHDocument;
 
 export type ReturnDocument<T extends Schema<any, any> | ParseSchema<any>, AF extends boolean = false> = T extends Schema<any, any, infer U>
     ? Document & MapSchema<U, AF>
-    : Document & MapSchema<T & ParseSchema<any>, AF>;
+    : T extends ParseSchema<any> ? Document & MapSchema<T, AF> : never;
