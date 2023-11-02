07779e0e5bfe9664c67b49fe877e823d84c5adad
DidaS <didasoficial@gmail.com>
2023-11-02 19:10:32 +0000

    Export reference array

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ export type * from "./model";
 /* The library exposes all its types but please use them only if you know what you are doing */
 export type * from "./typings";
 
+export * from "./utils/reference-array";
 export * from "./client";
